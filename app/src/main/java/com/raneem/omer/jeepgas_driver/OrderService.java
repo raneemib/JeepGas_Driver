fb3862b75c4ed187e6bed651af36b18ea3750c4d
Raneem <ramenib@gmail.com>
2017-06-28 11:06:11 +0300

    bug fixes cleaning the code removing any code within comments

--- app/src/main/java/com/raneem/omer/jeepgas_driver/OrderService.java
+++ app/src/main/java/com/raneem/omer/jeepgas_driver/OrderService.java
@@ -195,7 +195,7 @@ public class OrderService extends Service {
 
                 .setSmallIcon(R.drawable.cast_ic_notification_small_icon)
                 .setContentTitle("JeebGas-Drivers")
-                .setContentText("An Order Has Been Canceled")
+                .setContentText("An Order Has Been Deleted")
                 .build();
 
         mBuilder.flags |= Notification.FLAG_AUTO_CANCEL;
